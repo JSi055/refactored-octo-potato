dbb3e5e475cabd1a461dee0c25e395eaf285e45d
xcube <xcube16@gmail.com>
2022-12-11 16:17:43 -0600

    added linear interpolation calibration thingy

---
+++ Analizer_driver/calibration.py
@@ -0,0 +1,55 @@
+
+class Point:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+
+class CalCurve:
+    _points = [] # [point, ...]
+
+    # FIXME: it is assumed that x is increasing each point!
+    def x_to_y(self, x) -> float:
+        if len(self.points) < 2:
+            return 0 # unknown slope. we need at least 2 points!
+        point1: Point = self.points[0]
+        point2: Point = self.points[1]
+
+        # check if we are below the range
+        for i in range(len(self.points) - 1):
+            point1 = self.points[i]
+            point2 = self.points[i + 1]
+            if x <= point2.x:
+                break
+
+        # y = mx + b # linear interpolation
+        return ((x - point1.x) * (point2.y - point1.y)) / (point2.x - point1.x) + point1.y
+
+    # FIXME: it is assumed that y is increasing each point!
+    def y_to_x(self, y) -> float:
+        if len(self.points) < 2:
+            return 0 # unknown slope. we need at least 2 points!
+        point1: Point = self.points[0]
+        point2: Point = self.points[1]
+
+        # check if we are below the range
+        for i in range(len(self.points) - 1):
+            point1 = self.points[i]
+            point2 = self.points[i + 1]
+            if y <= point2.y:
+                break
+
+        # x = my + b # linear interpolation
+        return ((y - point1.y) * (point2.x - point1.x)) / (point2.y - point1.y) + point1.x
+
+    @property
+    def points(self):
+        return self._points
--- Analizer_driver/database.py
+++ Analizer_driver/database.py
@@ -1,12 +1,43 @@
 
 import sqlite3
+import time
 
 sql_con: sqlite3.Connection
+sql_cur: sqlite3.Cursor
+
+start_time_seconds: int
+last_commit_time: int
 
 def loadDatabase(dbFile) -> sqlite3.Connection:
     global sql_con
+    global sql_cur
     sql_con = sqlite3.connect(dbFile)
 
-    cursor = sql_con.cursor()
-    cursor.execute("CREATE TABLE loadTest()")
+    sql_cur = sql_con.cursor()
+    sql_cur.execute("CREATE TABLE loadTest(time integer, value float)")
+    sql_con.commit()
 
+    last_commit_time = time.time() * 1000
+    resetVoltageTime()
+
+def resetVoltageTime():
+    start_time_seconds = round(time.time())
+
+def putVoltage(voltage: float):
+    global last_commit_time
+
+    t = time.time()
+    timeDelta = start_time_seconds - round(t)
+
+    sql_cur.execute("INSERT INTO loadTest (time, value) VALUES (" + timeDelta + ", " + voltage + ")")
+
+    # commit every 3 seconds
+    if t - last_commit_time > 3:
+        sql_con.commit()
+        last_commit_time = round(t)
+
+
+def close():
+
+    sql_con.commit()
+    sql_con.close()
